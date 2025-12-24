b59bfd3f6504fe55e562b38a007bf9c575a9b9f0
Tawfeeq <tawfeeq88700@gmail.com>
2025-12-24 20:11:08 +0530

    feat:Changed entire font and Ui Added animation in dashboard

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+
+import App from "./App";
+import "./App.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Toaster position="top-center" />
+    <App />
+  </>
+);
